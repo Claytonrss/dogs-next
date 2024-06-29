4f6fc6866b3fb035627f7271e28ee8c00c8ae701
Clayton Rafael <clayton.rssouza@gmail.com>
2024-06-29 18:08:58 -0300

    refactor: data-testid

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -2,30 +2,37 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import Header from '.';
 
-describe('Header Component', () => {
-  it('renders correctly', () => {
+describe('Componente Header', () => {
+  it('renderiza corretamente', () => {
     const { container } = render(<Header />);
     expect(container).toBeInTheDocument();
   });
 
-  it('renders the logo link', () => {
+  it('renderiza o link do logo', () => {
     render(<Header />);
     const logoLink = screen.getByRole('link', { name: /Dogs - Home/i });
     expect(logoLink).toBeInTheDocument();
   });
 
-  it('renders links with appropriate aria-labels', () => {
+  it('renderiza links com os atributos aria-label apropriados', () => {
     render(<Header />);
     const logoLink = screen.getByRole('link', { name: /Dogs - Home/i });
     expect(logoLink).toHaveAttribute('aria-label', 'Dogs - Home');
   });
 
-  it('renders the logo image', () => {
+  it('renderiza a imagem do logo', () => {
     render(<Header />);
     const logoImage = screen.getByAltText('Dogs - Home');
     expect(logoImage).toBeInTheDocument();
     expect(logoImage).toHaveAttribute('src', '/assets/dogs.svg');
     expect(logoImage).toHaveAttribute('width', '28');
     expect(logoImage).toHaveAttribute('height', '22');
+  });
+
+  it('renderiza o link para "conta" quando o usuário está logado', () => {
+    render(<Header />);
+    const accountLink = screen.getByTestId('link-to-account');
+    expect(accountLink).toBeInTheDocument();
+    expect(accountLink).toHaveAttribute('href', '/conta');
   });
 });
